d5403072d8884caf07a2293a42be569a4483154e
khushi1997n <40892595+khushi1997n@users.noreply.github.com>
2021-04-28 11:29:15 +0530

    Add files via upload

---
+++ finalscript.ps1
@@ -0,0 +1,11 @@
+Write-Output "Downloading filebeat-Zip"
+Invoke-WebRequest https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.12.0-windows-x86_64.zip -Outfile filebeat.zip
+Write-Output "Unzipping file..."
+Expand-Archive filebeat.zip -destinationPath C:\Users\khushboo\Desktop
+$body = "C:\Users\khushboo\Desktop\filebeat-7.12.0-windows-x86_64"
+Set-Location "$body"
+powershell.exe -ExecutionPolicy UnRestricted -File .\install-service-filebeat.ps1
+.\filebeat.exe -e test config
+.\filebeat.exe -c filebeat.yml -e 
+Start-Service filebeat
+Stop-Service filebeat
