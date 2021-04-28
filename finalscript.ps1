1ce72e59f280d9c19111b604554d43173c0cc163
khushi1997n <40892595+khushi1997n@users.noreply.github.com>
2021-04-28 17:00:25 +0530

    Update finalscript.ps1

--- finalscript.ps1
+++ finalscript.ps1
@@ -8,5 +8,5 @@ powershell.exe -ExecutionPolicy UnRestricted -File .\install-service-filebeat.ps
 .\filebeat.exe -e test config
 .\filebeat.exe -c filebeat.yml -e 
 Start-Service filebeat
-filebeat.shutdown_timeout: 15s
+filebeat.shutdown_timeout: 120s
 Stop-Service filebeat
