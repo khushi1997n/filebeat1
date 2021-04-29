050a8146178b06727d7997a5eedee5634aa81169
khushi1997n <40892595+khushi1997n@users.noreply.github.com>
2021-04-29 17:34:15 +0530

    Update finalscript.ps1

--- finalscript.ps1
+++ finalscript.ps1
@@ -1,7 +1,7 @@
 Write-Output "Downloading filebeat-Zip"
 Invoke-WebRequest https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.12.0-windows-x86_64.zip -Outfile filebeat.zip
 Write-Output "Unzipping file..."
-Expand-Archive filebeat.zip -destinationPath C:\Users\khushboo\Desktop
+Expand-Archive filebeat.zip -destinationPath C:\Users\khushboo\Desktop\pipeline
 $body = "C:\Users\khushboo\Desktop\filebeat-7.12.0-windows-x86_64"
 Set-Location "$body"
 powershell.exe -ExecutionPolicy UnRestricted -File .\install-service-filebeat.ps1
