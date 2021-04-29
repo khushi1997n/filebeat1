5c71f3b9f3208e0f1cdf631b0062f31a914b79da
khushi1997n <40892595+khushi1997n@users.noreply.github.com>
2021-04-29 18:00:30 +0530

    Update finalscript.ps1

--- finalscript.ps1
+++ finalscript.ps1
@@ -4,15 +4,10 @@ Write-Output "Unzipping file..."
 Expand-Archive filebeat.zip -destinationPath C:\Users\khushboo\Downloads
 $body = "C:\Users\khushboo\Downloads\filebeat-7.12.0-windows-x86_64"
 Set-Location "$body"
-Write-Output "Service Executing..."
 powershell.exe -ExecutionPolicy UnRestricted -File .\install-service-filebeat.ps1
-Write-Output "Filebeat Configuration..."
 .\filebeat.exe -e test config
-Write-Output "Filebeat Execution..."
 .\filebeat.exe -c filebeat.yml -e 
-Write-Output "Filebeat Starting..."
 Start-Service filebeat
-Write-Output "Powershell Running in Background..."
-powershell -File finalscript.ps1 -WindowStyle Hidden
+
 
 
