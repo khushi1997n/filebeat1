b1606c4238762cc8c842cc53c4353e0a10216a16
khushi1997n <40892595+khushi1997n@users.noreply.github.com>
2021-04-30 18:17:11 +0530

    Update finalscript.ps1

--- finalscript.ps1
+++ finalscript.ps1
@@ -1,18 +1,14 @@
 Write-Output "Downloading filebeat-Zip"
 Invoke-WebRequest https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.12.0-windows-x86_64.zip -Outfile filebeat.zip
 Write-Output "Unzipping file..."
-Expand-Archive filebeat.zip -destinationPath C:\Users\khushboo\Downloads\ppp
-$body = "C:\Users\khushboo\Downloads\ppp\filebeat-7.12.0-windows-x86_64"
+Expand-Archive filebeat.zip -destinationPath C:\Users\khushboo\Desktop\kkk
+remove-item C:\Users\khushboo\Desktop\kkk\filebeat-7.12.0-windows-x86_64\filebeat.yml
+Copy-Item 'C:\Users\khushboo\Desktop\filebeatnew\filebeat.yml' 'C:\Users\khushboo\Desktop\kkk\filebeat-7.12.0-windows-x86_64'
+$body = "C:\Users\khushboo\Desktop\kkk\filebeat-7.12.0-windows-x86_64"
 Set-Location "$body"
-Write-Output "Service Executing..."
 powershell.exe -ExecutionPolicy UnRestricted -File .\install-service-filebeat.ps1
-Write-Output "Filebeat Configuration..."
 .\filebeat.exe -e test config
-Write-Output "Filebeat Execution..."
-.\filebeat.exe -c filebeat.yml -e 
 Start-Service filebeat
-powershell.exe -windowstyle hidden -file C:\Users\khushboo\Desktop
-
 
 
 
