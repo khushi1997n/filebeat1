18125ce9261c7256b9e09c75fc73d9639611a365
khushi1997n <40892595+khushi1997n@users.noreply.github.com>
2021-04-30 19:14:44 +0530

    Update finalscript.ps1

--- finalscript.ps1
+++ finalscript.ps1
@@ -1,10 +1,10 @@
 Write-Output "Downloading filebeat-Zip"
 Invoke-WebRequest https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.12.0-windows-x86_64.zip -Outfile filebeat.zip
 Write-Output "Unzipping file..."
-Expand-Archive filebeat.zip -destinationPath C:\Users\khushboo\Desktop\kkk
-remove-item C:\Users\khushboo\Desktop\kkk\filebeat-7.12.0-windows-x86_64\filebeat.yml
-Copy-Item 'C:\Users\khushboo\Desktop\filebeatnew\filebeat.yml' 'C:\Users\khushboo\Desktop\kkk\filebeat-7.12.0-windows-x86_64'
-$body = "C:\Users\khushboo\Desktop\kkk\filebeat-7.12.0-windows-x86_64"
+Expand-Archive filebeat.zip -destinationPath C:\Users\khushboo\Desktop\k
+remove-item C:\Users\khushboo\Desktop\k\filebeat-7.12.0-windows-x86_64\filebeat.yml
+Copy-Item 'C:\Users\khushboo\Desktop\filebeatnew\filebeat.yml' 'C:\Users\khushboo\Desktop\k\filebeat-7.12.0-windows-x86_64'
+$body = "C:\Users\khushboo\Desktop\k\filebeat-7.12.0-windows-x86_64"
 Set-Location "$body"
 powershell.exe -ExecutionPolicy UnRestricted -File .\install-service-filebeat.ps1
 .\filebeat.exe -e test config
