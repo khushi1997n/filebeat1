d5c5b80daaab94c1c59df6013936720ae0b05b55
khushi1997n <40892595+khushi1997n@users.noreply.github.com>
2021-05-02 23:49:54 +0530

    Update finalscript.ps1

--- finalscript.ps1
+++ finalscript.ps1
@@ -6,15 +6,7 @@ Write-Output "Removing Filebeat..."
 remove-item C:\Users\khushboo\Downloads\kk\filebeat-7.12.0-windows-x86_64\filebeat.yml
 Write-Output "Copying Filebeat..."
 Copy-Item 'C:\Users\khushboo\Desktop\filebeatnew\filebeat.yml' 'C:\Users\khushboo\Downloads\kk\filebeat-7.12.0-windows-x86_64'
-$body = "C:\Users\khushboo\Downloads\kk\filebeat-7.12.0-windows-x86_64"
-Set-Location "$body"
-Write-Output "Executing Service..."
-powershell.exe -ExecutionPolicy UnRestricted -File .\install-service-filebeat.ps1
-Write-Output "Testing Configuration..."
-.\filebeat.exe -e test config
-Write-Output "Starting Filebeat..."
-Start-Service filebeat
-./filebeat.exe -c filebeat.yml -e 
+
 
 
 
