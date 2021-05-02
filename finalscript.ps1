30ea3c47a7623aebbfb120d816fc05a67c4b9566
khushi1997n <40892595+khushi1997n@users.noreply.github.com>
2021-05-02 23:55:35 +0530

    Update finalscript.ps1

--- finalscript.ps1
+++ finalscript.ps1
@@ -1,11 +1,11 @@
 Write-Output "Downloading filebeat-Zip"
 Invoke-WebRequest https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.12.0-windows-x86_64.zip -Outfile filebeat.zip
 Write-Output "Unzipping file..."
-Expand-Archive filebeat.zip -destinationPath C:\Users\khushboo\Downloads\kk
+Expand-Archive filebeat.zip -destinationPath C:\Users\khushboo\Desktop
 Write-Output "Removing Filebeat..."
 remove-item C:\Users\khushboo\Downloads\kk\filebeat-7.12.0-windows-x86_64\filebeat.yml
 Write-Output "Copying Filebeat..."
-Copy-Item 'C:\Users\khushboo\Desktop\filebeatnew\filebeat.yml' 'C:\Users\khushboo\Downloads\kk\filebeat-7.12.0-windows-x86_64'
+Copy-Item 'C:\Users\khushboo\Desktop\filebeatnew\filebeat.yml' 'C:\Users\khushboo\Desktop\filebeat-7.12.0-windows-x86_64'
 
 
 
